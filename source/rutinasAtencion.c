d4286b1c212fcb46cafceea96024677c838b56bc
0rtzi <udorburu@gmail.com>
2026-04-07 14:48:39 +0200

    Implementación de randomInt y generación aleatoria de setas.

--- source/rutinasAtencion.c
+++ source/rutinasAtencion.c
@@ -14,19 +14,32 @@ rutinasAtencion.c
 
 //Definición de variables globales.
 int ESTADO; // Para controlar el estado del autómata en que esté
+int tick = 0;
+static unsigned int semilla_interna = 12345;
+
+int randomInt(int min, int max) {
+	semilla_interna = semilla_interna * 1103515245 + 12345;
+
+	unsigned int numAleatorio = (semilla_interna / 65536) % 32768;
+    int dif = max-min+1;
+    return min+ (numAleatorio % dif);
+}
+
 
 	//PROTA
 struct protagonista prota;
 int prota_cont_espera_mov = 0;
-int prota_cont_espera_mov_min = 2;
-int prota_pixel_mov = 4;
+int prota_cont_espera_mov_min = 1;
+int prota_pixel_mov = 2;
 
 void InicializarValoresProta(){
 	prota.vidas=3;
-	prota.X=0;
-	prota.Y=0;
+	prota.X=128;
+	prota.Y=160;
+	prota.puntos=0;
 	prota_cont_espera_mov = 0;
-	MostrarProta(1, prota.Y, prota.X);
+
+	MostrarProta(0, prota.X, prota.Y);
 }
 
 // Actualiza la posición del protagonista
@@ -35,37 +48,55 @@ void ActualizarPosicionProta() {
 		prota_cont_espera_mov=0;
 		int tecla = TeclaPulsada();
 		if (tecla==ARRIBA){
-			if (prota.Y>0){
-				BorrarProta(1, prota.X, prota.Y);
+			if (prota.Y>144){
+				BorrarProta(0, prota.X, prota.Y);
 				prota.Y-=prota_pixel_mov;
-				MostrarProta(1, prota.X, prota.Y);
+				MostrarProta(0, prota.X, prota.Y);
 			}
 		}
 		else if (tecla==ABAJO){
-			if (prota.Y<192){
-				BorrarProta(1, prota.X, prota.Y);
+			if (prota.Y<176){
+				BorrarProta(0, prota.X, prota.Y);
 				prota.Y+=prota_pixel_mov;
-				MostrarProta(1, prota.X, prota.Y);
+				MostrarProta(0, prota.X, prota.Y);
 			}
 		}
 		else if (tecla==IZQUIERDA){
 			if (prota.X>0){
-				BorrarProta(1, prota.X, prota.Y);
+				BorrarProta(0, prota.X, prota.Y);
 				prota.X-=prota_pixel_mov;
-				MostrarProta(1, prota.X, prota.Y);
+				MostrarProta(0, prota.X, prota.Y);
 			}
 		}
 		else if (tecla==DERECHA){
 			if (prota.X<240){
-				BorrarProta(1, prota.X, prota.Y);
+				BorrarProta(0, prota.X, prota.Y);
 				prota.X+=prota_pixel_mov;
-				MostrarProta(1, prota.X, prota.Y);
+				MostrarProta(0, prota.X, prota.Y);
 			}
 		}
 		
-		iprintf("\x1b[23;5HX: %d  Y: %d",prota.X, prota.Y);
+		//iprintf("\x1b[23;5HX: %d  Y: %d",prota.X, prota.Y);
 	}
     
+}
+
+	// SETAS
+int matriz_setas[12][16];
+
+void InicializarValoresSetas() {
+	int ultNumAsig=2;
+	for (int i=0; i<9; i++){
+		for (int j=0; j<16; j++){
+			if (randomInt(0,7)==0){
+				matriz_setas[i][j]=4;
+				MostrarChampi(ultNumAsig,j*16,i*16);
+				ultNumAsig++;
+			}
+			
+		}
+	}
+
 }
 
 //RUTINAS DE ATENCIÓN
@@ -112,14 +143,21 @@ void RutAtencionTeclado ()
 	// }
 
 	if (TeclaPulsada() == A){
+		InhibirIntTeclado();
 		visualizarFondoPrueba();
 		InicializarValoresProta();
-		PonerEnMarchaTempo();
+		InicializarValoresSetas();
+		HabilitarIntTeclado();
 	}
 }
 
 void RutAtencionTempo()
 {
+	tick++;
+	if (tick >128){
+		tick = 0;
+	}
+
 	if (ESTADO==PARTIDA){
 		if (prota_cont_espera_mov < prota_cont_espera_mov_min){
 			prota_cont_espera_mov++;
