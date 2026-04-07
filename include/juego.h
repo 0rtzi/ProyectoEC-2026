10ce65c0a2d37b0bb684af06fcf3f37b625511bd
0rtzi <udorburu@gmail.com>
2026-04-07 14:49:19 +0200

    Declaración de randomInt() y Matriz de setas

--- include/juego.h
+++ include/juego.h
@@ -5,11 +5,15 @@ juego.h
 ---------------------------------------------------------------------------------*/
 void juego();
 
+// Random
+int randomInt(int min, int max);
+
 //PROTAGONISTA
 struct protagonista {
     int vidas;
     int X;
     int Y;
+    int puntos;
 };
 
 extern int prota_cont_espera_mov;
@@ -21,6 +25,13 @@ extern struct protagonista prota;
 extern void InicializarValoresProta();
 
 extern void ActualizarPosicionProta();
+
+
+// SETAS
+extern int matriz_setas[12][16];
+
+extern void InicializarValoresSetas();
+
 
 
 /***********************2025-2026*******************************/
